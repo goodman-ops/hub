120f009dec040c8a5a235556662195f0a80ffae0
sisou <hello@soerenschwert.de>
2019-09-16 22:25:29 +0200

    Revert local network endpoint config

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -3,7 +3,7 @@ import { NETWORK_TEST } from '../lib/Constants';
 export default {
     keyguardEndpoint: window.location.protocol + '//' + window.location.hostname + ':8000/src',
     network: NETWORK_TEST,
-    networkEndpoint: 'http://localhost:8001',
+    networkEndpoint: 'https://network.nimiq-testnet.com',
     privilegedOrigins: [ '*' ],
     redirectTarget: window.location.protocol + '//' + window.location.hostname + ':8080/demos.html',
     reportToSentry: false,
