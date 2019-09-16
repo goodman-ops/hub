73c8c775ae091ccb0d1a4772ef369ba37847a012
Daniel <github.danimoh@neverbox.com>
2019-09-16 22:47:40 +0200

    [Deployment Fix]: diplay amount without fee in NonNimiqCheckoutOption

--- src/components/NonNimiqCheckoutOption.vue
+++ src/components/NonNimiqCheckoutOption.vue
@@ -58,13 +58,13 @@
                 <div class="qr-code">
                 </div>
                 <div class="copyable-payment-information">
-                    <Copyable :text="paymentOptions.displayableAmount">
+                    <Copyable>
                         <UniversalAmount class="nq-link payment-link"
                             :decimals="paymentOptions.currency === Currency.BTC ? 8 : 18"
                             :minDecimals="paymentOptions.currency === Currency.BTC ? 8 : 18"
                             :maxDecimals="paymentOptions.currency === Currency.BTC ? 8 : 18"
                             :currency="paymentOptions.currency"
-                            :amount="paymentOptions.total" />
+                            :amount="paymentOptions.amount" />
                         <!--a class="nq-link payment-link">{{paymentOptions.total}} {{paymentOptions.currency}}</a-->
                     </Copyable>
                     <Copyable>
